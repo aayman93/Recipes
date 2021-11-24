0ed57f25cd923ad8edeb46fc076bddb98c7f5336
aayman93 <ahmedaymann93@gmail.com>
2021-11-24 19:36:16 +0200

    Setup Navigation

---
+++ app/src/main/java/com/github/aayman93/recipes/ui/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.github.aayman93.recipes.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.github.aayman93.recipes.R
+import com.github.aayman93.recipes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNav.apply {
+            setupWithNavController(navController)
+            setOnItemReselectedListener { }
+        }
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.findRecipeFragment,
+                R.id.favouritesFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+    }
+}
---
+++ app/src/main/java/com/github/aayman93/recipes/ui/fragments/FavouritesFragment.kt
@@ -0,0 +1,28 @@
+package com.github.aayman93.recipes.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.aayman93.recipes.databinding.FragmentFavouritesBinding
+
+class FavouritesFragment : Fragment() {
+
+    private var _binding: FragmentFavouritesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFavouritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
